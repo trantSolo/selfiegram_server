b49c113f4ac16283b80201bd3fec219ea86409ba
Trant Solo <trant@ip-192-168-1-49.is74.loc>
2017-09-07 15:26:52 +0000

    Initial Commit

---
+++ Selfiegram_server/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Selfiegram_server
+//
+//  Created by Trant Solo on 07.09.17.
+//  Copyright © 2017 tuchka. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Selfiegram_server/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Selfiegram_server
+//
+//  Created by Trant Solo on 07.09.17.
+//  Copyright © 2017 tuchka. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
